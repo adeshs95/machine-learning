8e8c04b919e7cd61ba978b9da1f9ffc7e36ab423
adeshs95 <73020130+adeshs95@users.noreply.github.com>
2020-11-22 11:40:47 +0530

    Update Decision_tree.py

--- Decision_tree.py
+++ Decision_tree.py
@@ -1,19 +1,3 @@
-# Dataset :Breast Cancer dataset
-# Downlaod link : https://archive.ics.uci.edu/ml/datasets/Breast+Cancer
-
-
-"""1. Build a decision tree by taking as input a maximum depth and by randomly splitting the 
-dataset as 80/20 split i.e., 80% for training and 20% for testing. Provide the accuracy by 
-averaging over 10 random 80/20 splits. Consider that particular tree which provides the 
-best test accuracy as the desired one. 
-
-2. What is the best possible depth limit to be used for your dataset. Provide a plot 
-explaining the same. 
-3. Perform the pruning operation over the tree obtained in question 2 using a valid 
-statistical test for comparison. 
-4. Print the final decision tree obtained from question 3 following the hierarchical levels of 
-data attributes as nodes of the tree."""
-
 import numpy as np
 import pandas as pd
 
